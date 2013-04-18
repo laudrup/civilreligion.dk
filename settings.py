7fae3376f36b0ef652ba47d88af08d13a26e963d
Kasper Laudrup <laudrup@stacktrace.dk>
2013-04-18 15:34:58 +0200

    The homepage is in Danish so set the language code to that.

--- settings.py
+++ settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "dk"
+LANGUAGE_CODE = "da-DK"
 
 # A boolean that turns on/off debug mode. When set to ``True``, stack traces
 # are displayed for error pages. Should always be set to ``False`` in
